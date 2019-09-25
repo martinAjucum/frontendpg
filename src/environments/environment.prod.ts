3704758118e1fa04d803e974d407a9a7b13092ca
Martin <martin_aj22@hotmail.com>
2019-09-24 22:04:20 -0600

    firebase configured

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAnN35AI86n5AKBoOA6IsVKQr9GqXY0KKQ",
+    authDomain: "alicegproject2.firebaseapp.com",
+    databaseURL: "https://alicegproject2.firebaseio.com",
+    projectId: "alicegproject2",
+    storageBucket: "",
+    messagingSenderId: "37738620895",
+    appId: "1:37738620895:web:ba36253d6a58c6c502866a"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAnN35AI86n5AKBoOA6IsVKQr9GqXY0KKQ",
+    authDomain: "alicegproject2.firebaseapp.com",
+    databaseURL: "https://alicegproject2.firebaseio.com",
+    projectId: "alicegproject2",
+    storageBucket: "",
+    messagingSenderId: "37738620895",
+    appId: "1:37738620895:web:ba36253d6a58c6c502866a"
+  }
 };
 
 /*
