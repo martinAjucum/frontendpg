1be78d99b32f1326e2ae00c08ed6c696dfddc4d9
Martin <martin_aj22@hotmail.com>
2019-10-15 01:37:16 -0600

    Se modifico pagina privada y se cambiaron datos del json del grafico

--- src/app/componentes/grafico/grafico.component.ts
+++ src/app/componentes/grafico/grafico.component.ts
@@ -10,7 +10,7 @@ export class GraficoComponent implements OnInit {
   chartConfig: Object;
   constructor() { 
     this.chartConfig = {
-      width: '700',
+      width: '400',
       height: '400',
       type: 'column2d',
       dataFormat: 'json',
@@ -18,37 +18,19 @@ export class GraficoComponent implements OnInit {
   
    this.dataSource = {
             "chart": {
-              "caption": "Countries With Most Oil Reserves [2017-18]",
-              "subCaption": "In MMbbl = One Million barrels",
-              "xAxisName": "Country",
-              "yAxisName": "Reserves (MMbbl)",
-              "numberSuffix": "K",
+              "caption": "Resultados",
+              "subCaption": "Deteccion de neumonia con Deep Learning",
+              "xAxisName": "Estado",
+              "yAxisName": "Probabilidad",
+              "numberSuffix": "%",
               "theme": "fusion",
             },
             "data": [{
-              "label": "Venezuela",
-              "value": "290"
+              "label": "NORMAL",
+              "value": "86"
             }, {
-              "label": "Saudi",
-              "value": "260"
-            }, {
-              "label": "Canada",
-              "value": "180"
-            }, {
-              "label": "Iran",
-              "value": "140"
-            }, {
-              "label": "Russia",
-              "value": "115"
-            }, {
-              "label": "UAE",
-              "value": "100"
-            }, {
-              "label": "US",
-              "value": "30"
-            }, {
-              "label": "China",
-              "value": "30"
+              "label": "PNEUMONIA",
+              "value": "14"
             }]
           };
   }
